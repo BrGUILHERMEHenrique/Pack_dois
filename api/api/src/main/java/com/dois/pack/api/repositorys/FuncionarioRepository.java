53356bca0f8030a4d1471ef009050e10eec42c31
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-20 08:59:23 -0300

    Criando: Query buscando por matricula

--- api/api/src/main/java/com/dois/pack/api/repositorys/FuncionarioRepository.java
+++ api/api/src/main/java/com/dois/pack/api/repositorys/FuncionarioRepository.java
@@ -2,10 +2,15 @@ package com.dois.pack.api.repositorys;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.dois.pack.api.models.Funcionario;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
 
+	@Query(value = "SELECT * FROM funcionario where cod_matricula = :codMatricula",
+			nativeQuery = true)
+	Funcionario getWithMatricula(String codMatricula);
 }
