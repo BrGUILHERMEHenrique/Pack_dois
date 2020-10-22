878784a07b56b5fc26c0e58b1f4a328ed29f3190
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-22 11:59:42 -0300

    Adicionando: rota horarios

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -5,13 +5,15 @@ import Funcionarios from '../pages/Funcionarios';
 import Horario from '../pages/Horario';
 import Empresas from '../pages/Empresas';
 import HorarioDetalhes from '../pages/HorarioDetalhes';
+import HorarioTabela from '../pages/HorarioTabela';
 
 
 const Routes = () => (
     <Switch>
 
         <Route exact path="/" component={Funcionarios} />
-        <Route exact path="/horario" component={HorarioDetalhes} />
+        <Route exact path="/horarioDetalhes" component={HorarioDetalhes} />
+        <Route exact path="/horarios" component={HorarioTabela} />
         <Route exact path="/funcionarios" component={Funcionarios} />
         <Route exact path="/empresas" component={Empresas} />
         
